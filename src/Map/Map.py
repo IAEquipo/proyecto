eb7f90f0893cd41b08df2ee6ef2cf0786fe19d3a
ersajo <sanchezjoserick@gmail.com>
2018-06-18 21:29:58 -0500

    Reestructura parcial funcional, falta merge para confirmar

--- src/Map/Map.py
+++ src/Map/Map.py
@@ -4,14 +4,14 @@
 from Archivo.Archivo import *
 
 class Map(object):
-    __dimensions = []
 
     def __init__(self):
         text = Archivo()
+        self.__dimensions = list()
         self.__matrix = text.read('lab2.txt')
+        self.__dimensions.append(len(self.__matrix))
         self.__dimensions.append(len(self.__matrix[0]))
-        self.__dimensions.append(len(self.__matrix)-1)
-        self.__darkside = [[-1 for j in range(self.__dimensions[0])] for i in range(self.__dimensions[1])]
+        #self.__darkside = [[-1 for j in range(self.__dimensions[0])] for i in range(self.__dimensions[1])]
 
     def getDimensions(self):
         return self.__dimensions
@@ -19,5 +19,13 @@ class Map(object):
     def getMatrix(self):
         return self.__matrix
 
-    def getDarkSide(self):
-        return self.__darkside
+    """def getDarkSide(self):
+        return self.__darkside"""
+
+    def setVal(self, pos, val):
+        px = pos[0]
+        py = pos[1]
+        self.__matrix[px][py] = val
+
+    def validate(self, start, tipo):
+        return self.__matrix[start[0]][start[1]] != '0'
