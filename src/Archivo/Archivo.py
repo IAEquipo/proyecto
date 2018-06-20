9b06691424d76302640f967aa88bbb2d8b185f97
ersajo <sanchezjoserick@gmail.com>
2018-06-19 21:37:13 -0500

    Se crearon los metodos de Map

--- src/Archivo/Archivo.py
+++ src/Archivo/Archivo.py
@@ -19,6 +19,16 @@ class Archivo:
 
         return out
 
+    def read_being(self, ruta):
+        with open(ruta, 'r') as leer:
+            contenido = leer.read().split('\n')
+        i = 0
+        for line in contenido:
+            contenido[i] = line.split(',')
+            i = i + 1
+        leer.close()
+        return contenido
+
     def write(self, ruta, contenido):
         escribir = open(ruta , 'w')
         escribir.write(contenido)
--- src/Map/Map.py
+++ src/Map/Map.py
@@ -6,26 +6,48 @@ from Archivo.Archivo import *
 class Map(object):
 
     def __init__(self):
-        text = Archivo()
+        file = Archivo()
         self.__dimensions = list()
-        self.__matrix = text.read('lab2.txt')
+        self.__matrix = file.read('lab2.txt')
         self.__dimensions.append(len(self.__matrix))
         self.__dimensions.append(len(self.__matrix[0]))
+        self.__beingInfo = {}
+        file = Archivo()
+        being_info = file.read('beings.txt')
+        for line in being_info:
+            self.__beingInfo[line[0]] = line[1:]
         #self.__darkside = [[-1 for j in range(self.__dimensions[0])] for i in range(self.__dimensions[1])]
 
+    @property
     def getDimensions(self):
         return self.__dimensions
 
+    @property
     def getMatrix(self):
         return self.__matrix
 
-    """def getDarkSide(self):
-        return self.__darkside"""
+    @property
+    def getDarkSide(self):
+        return self.__darkside
+
+    def getUp(self, pos):
+        return self.__matrix[pos[0]][pos[1]-1]
+
+    def getDown(self, pos):
+        return self.__matrix[pos[0]][pos[1]+1]
+
+    def getRight(self, pos):
+        return self.__matrix[pos[0]+1][pos[1]]
+
+    def getLeft(self, pos):
+        return self.__matrix[pos[0]-1][pos[1]]
 
     def setVal(self, pos, val):
         px = pos[0]
         py = pos[1]
         self.__matrix[px][py] = val
 
-    def validate(self, start, tipo):
-        return self.__matrix[start[0]][start[1]] != '0'
+    def valTerrain(self, pos, tipo):
+        vals = self.__beingInfo.get(tipo)
+        terrain = int(self.__matrix[pos[0]][pos[1]])
+        return (vals[terrain] != 'X')
